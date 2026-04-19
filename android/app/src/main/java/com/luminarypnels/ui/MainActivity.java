4cf163756b57e1ec277548a9cd3fd9b2483a8f8d
firefly-sylestia <khanquadir19@gmail.com>
2026-04-19 14:05:56 +0530

    Revamp mobile editor controls, color picker UX, and in-app project saves

--- android/app/src/main/java/com/luminarypnels/ui/MainActivity.java
+++ android/app/src/main/java/com/luminarypnels/ui/MainActivity.java
@@ -1,5 +1,16 @@
 package com.luminarypnels.ui;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.view.WindowCompat;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+    }
+}
